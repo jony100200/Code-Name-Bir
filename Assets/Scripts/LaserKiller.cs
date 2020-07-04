e79558816f8956dfbd49b392090665e355af98e1
Jony <gamingbir@gmail.com>
2020-07-03 21:19:48 -0400

    fixed resolution
    fixed the resolution of game

--- Assets/Scripts/LaserKiller.cs
+++ Assets/Scripts/LaserKiller.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class LaserKiller : MonoBehaviour
 {
-    // Start is called before the first frame update
+   
     protected void OnTriggerEnter2D(Collider2D collision)
     {
         Destroy(collision.gameObject);
